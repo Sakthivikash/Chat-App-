9ff04bdd786c2f3cec62f975b222f193de72e79b
svikas <sakthivikash70@gmail.com>
2022-12-25 17:50:59 +0530

    Updated

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const io = require("socket.io")(server, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
+    transports: ["websocket", "polling", "flashsocket"],
   },
 });
 
